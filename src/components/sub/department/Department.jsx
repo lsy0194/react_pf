08d5a69ae776a7a3ced62565cb560492e7825d3c
lsy0194 <tktmadltmddu@naver.com>
2023-10-11 12:56:43 +0900

    department에 멤버 sns버튼 추가

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,6 +1,10 @@
 import Layout from '../../common/layout/Layout';
 import { useEffect, useState } from 'react';
 import './Department.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 const path = process.env.PUBLIC_URL;
 //1.useEffect로 컴포넌트마운트되자마자 fetch외부데이터 가져옴
 //2.데이터가 다 받아지면 useState로 state에 담아줌
@@ -21,12 +25,39 @@ export default function Department() {
 			<div className='memberBox'>
 				{Department.map((member, idx) => {
 					return (
-						<article key={idx}>
+						<article
+							key={idx}
+							onMouseOver={(e) => {
+								e.preventDefault();
+								e.currentTarget.classList.add('on');
+							}}
+							onMouseLeave={(e) => {
+								e.preventDefault();
+								e.currentTarget.classList.remove('on');
+							}}
+						>
 							<div className='pic'>
 								<img src={`${path}/img/${member.pic}`} alt={member.name} />
 							</div>
 							<h2>{member.name}</h2>
 							<p>{member.position}</p>
+							<div className='sns'>
+								<div className='btn'>
+									<a href='http://www.facebook.com'>
+										<FontAwesomeIcon icon={faFacebookF} className='facebook' />
+									</a>
+								</div>
+								<div className='btn'>
+									<a href='http://www.twitter.com'>
+										<FontAwesomeIcon icon={faXTwitter} className='twitter' />
+									</a>
+								</div>
+								<div className='btn'>
+									<a href='https://www.instagram.com/'>
+										<FontAwesomeIcon icon={faInstagram} className='instagram' />
+									</a>
+								</div>
+							</div>
 						</article>
 					);
 				})}
