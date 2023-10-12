eba9ddc3fd6e3c9de28803f7c0a3823743bf37a4
lsy0194 <tktmadltmddu@naver.com>
2023-10-12 13:13:47 +0900

    youtube 클래스 left에 꾸밈 문장추가

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -40,7 +40,21 @@ export default function Youtube() {
 			<Layout title={'Youtube'}>
 				<div className='underline'></div>
 				<div className='container'>
-					<div className='left'></div>
+					<div className='left'>
+						<h3>Lorem ipsum dolor sit.</h3>
+						<p className='subcontainer'>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus vitae, maxime
+							veritatis animi dolor tenetur dignissimos, adipisci nihil suscipit, consectetur
+							deleniti quod? Architecto cupiditate laborum delectus. Ratione ad fugit ipsam,
+							laboriosam adipisci et. Error non fugiat deleniti quos dicta, alias facilis officiis
+							et ad recusandae impedit ea quasi dolorum consequatur iste, odio quae, quod ex!
+							Fugiat, cumque doloribus amet quae quam quos. Harum ipsum, molestiae officiis, hic,
+							repellendus enim nostrum rerum voluptatibus quam impedit aliquid? Alias eligendi illum
+							voluptates ullam similique nisi voluptate in eveniet nihil, explicabo quisquam
+							repudiandae, inventore unde, recusandae minus iusto ratione autem illo magni nesciunt
+							sit?
+						</p>
+					</div>
 					{Youtube.map((data, idx) => {
 						let tit = data.snippet.title;
 						let desc = data.snippet.description;
